cb9a6f42556324d7bab1bc0ae6a5094b95b3c283
João Gabriel <joaoname9@gmail.com>
2024-12-28 11:27:59 -0300

    refactor: Header and About

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,45 +1,48 @@
 "use client";
 import Header from "@/components/header";
-import { Hero } from "@/components/ui/hero";
 import { Footer } from "@/components/footer";
 import TextGradientScroll from "@/components/ui/text-gradient";
-import Image from "next/image";
-import AdidasBanner from "@/app/assets/adidas-banner.png";
 import { TimelineDemo } from "@/components/timeline-demo";
 import HeroTitle from "@/components/ui/hero-title";
-import Typography from "@/components/ui/typography";
+import { ReactLenis } from "@studio-freight/react-lenis";
 
 export default function About() {
   return (
     <>
       <Header />
-      <main className="grid grid-rows-[auto_1fr_auto] grid-cols-1 gap-40">
-        {/* <Hero title="Sobre Mim" size="md" /> */}
-
-        <div className="container mx-auto max-w-6xl min-h-screen px-4 flex flex-col items-center justify-center space-y-4 text-center mt-[-4rem]">
+      <ReactLenis
+        root
+        options={{
+          lerp: 0.05,
+          //   infinite: true,
+          //   syncTouch: true,
+        }}
+      >
+        <main className="grid grid-rows-[auto_1fr_auto] grid-cols-1 gap-40">
+          <div className="container mx-auto max-w-6xl min-h-screen px-4 flex flex-col items-center justify-center space-y-4 text-center mt-[-4rem]">
             <HeroTitle text="Sobre" color="white" size="lg" />
             <HeroTitle text="Mim" color="silver" size="lg" />
-        </div>
+          </div>
 
-        <section className="w-full container mx-auto px-4">
-          <TextGradientScroll
-            className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center bg-clip-text  "
-            
-            text="Sou um Desenvolvedor Full Stack com mais de 2 anos de experiência em aplicações web, competente em React, React Native, Next.js, Node.js, TypeScript, MySQL, Docker e AWS. Habilidade na criação e manutenção de APIs, landing pages, portais de back office e lojas virtuais. Além do código, atuo no monitoramento de desempenho de aplicações utilizando New Relic."
-          />
-        </section>
+          <section className="w-full container mx-auto px-4">
+            <TextGradientScroll
+              className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center bg-clip-text  "
+              text="Sou um Desenvolvedor Full Stack com mais de 2 anos de experiência em aplicações web, competente em React, React Native, Next.js, Node.js, TypeScript, MySQL, Docker e AWS. Habilidade na criação e manutenção de APIs, landing pages, portais de back office e lojas virtuais. Além do código, atuo no monitoramento de desempenho de aplicações utilizando New Relic."
+            />
+          </section>
 
-        <section className="w-full container mx-auto px-4">
-          <video className="rounded-[3rem]" autoPlay loop muted>
-            <source src="/videos/aboutme.mp4" type="video/mp4" />
-            Seu navegador não suporta vídeos HTML5.
-          </video>
-        </section>
+          <section className="w-full container mx-auto px-4">
+            <video className="rounded-[3rem]" autoPlay loop muted>
+              <source src="/videos/aboutme.mp4" type="video/mp4" />
+              Seu navegador não suporta vídeos HTML5.
+            </video>
+          </section>
 
-        <section className="w-full min-h-screen container mx-auto px-4">
-          <TimelineDemo />
-        </section>
-      </main>
+          <section className="w-full min-h-screen container mx-auto px-4">
+            <TimelineDemo />
+          </section>
+        </main>
+      </ReactLenis>
       <Footer />
     </>
   );
--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { GiWolfHead } from "react-icons/gi";
-import { TfiClose } from "react-icons/tfi";
 import { CgMenuMotion, CgClose } from "react-icons/cg";
 import MagneticButton from "./ui/button-magnetic";
 import { FlipLink } from "./ui/reveal-links";
@@ -10,65 +9,63 @@ import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
 import { Button } from "./ui/button";
 
-type NavItem = {
-  id: number;
-  title: string;
-  href: string;
-};
-
-const NAV_ITEMS: NavItem[] = [
+const NAV_ITEMS = [
   { id: 0, title: "Home", href: "/" },
   { id: 1, title: "Sobre", href: "/about" },
   { id: 2, title: "Contato", href: "/contact" },
 ];
 
-const Header: React.FC = () => {
+const Header = () => {
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
   const mobileMenuVariant = {
     opened: { y: "0%", transition: { duration: 0.5, ease: "easeInOut" } },
-    closed: { y: "-100%", transition: { duration: 0.5, ease: "easeInOut" } },
+    closed: { y: "-100%", transition: { duration: 0, ease: "easeInOut" } },
   };
 
+  const toggleMobileNav = () => setMobileNavOpen((prev) => !prev);
+
   return (
-    <header className="container mx-auto px-4 pt-4 flex justify-between items-center relative">
+    <header className="container mx-auto px-4 py-4 flex justify-between items-center relative">
       {/* Logo */}
       <motion.div className="text-2xl font-bold uppercase relative z-50 cursor-pointer">
         <Link href="/">
-          <GiWolfHead className="text-7xl text-foreground" />
+          <GiWolfHead className="text-7xl text-foreground" aria-label="Logo" />
         </Link>
       </motion.div>
 
-      {/* Botão do menu */}
+      {/* Menu Toggle Button */}
       <motion.div
         className="relative z-50"
-        onClick={() => setMobileNavOpen(!mobileNavOpen)}
+        onClick={toggleMobileNav}
+        aria-label="Toggle navigation menu"
       >
         <MagneticButton
           distance={2}
           className="text-foreground border border-neutral-800 rounded-full p-5 cursor-pointer hover:bg-neutral-600 hover:bg-opacity-10"
         >
           {mobileNavOpen ? (
-            <CgClose className="text-4xl" />
+            <CgClose className="text-4xl" aria-label="Close menu" />
           ) : (
-            <CgMenuMotion className="text-4xl" />
+            <CgMenuMotion className="text-4xl" aria-label="Open menu" />
           )}
         </MagneticButton>
       </motion.div>
 
-      {/* Itens do Menu */}
+      {/* Mobile Navigation Menu */}
       <motion.div
         variants={mobileMenuVariant}
         animate={mobileNavOpen ? "opened" : "closed"}
-        className="fixed top-0 left-0 z-40 w-full h-screen flex bg-background flex-col items-center justify-center"
+        className="fixed top-0 left-0 z-40 w-full h-screen bg-background flex flex-col items-center justify-center"
       >
-        <motion.div className=" container mx-auto flex flex-col px-4 gap-10">
+        <motion.div className="container mx-auto flex flex-col px-4 gap-10">
           {NAV_ITEMS.map((item) => (
-            <FlipLink href={item.href}>{item.title}</FlipLink>
+            <FlipLink key={item.id} href={item.href} aria-label={item.title}>
+              {item.title}
+            </FlipLink>
           ))}
           <div className="flex flex-row items-center gap-4">
-
-          <ModeToggle />
-          <Button variant="outline">Português</Button>
+            <ModeToggle />
+            <Button variant="outline">Português</Button>
           </div>
         </motion.div>
       </motion.div>
