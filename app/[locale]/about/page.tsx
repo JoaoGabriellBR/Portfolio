0526eb90b956c87495717059084932fabecd0aa2
João Gabriel <joaoname9@gmail.com>
2025-03-31 09:24:22 -0300

    refactor: About

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -20,6 +20,7 @@ import { useTheme } from "next-themes";
 import { RiPokerSpadesFill } from "react-icons/ri";
 import ScrollBaseAnimation from "@/components/text-marquee";
 import { BsArrowDownLeft } from "react-icons/bs";
+import { DrawCircleText } from "@/components/draw-circle-text";
 
 export default function About() {
   const t = useTranslations("About");
@@ -71,7 +72,7 @@ export default function About() {
           </section>
 
           {/* Experience */}
-          <section className={`${containerStyles}`}>
+          {/* <section className={`${containerStyles}`}>
             <Typography
               text={t("Experience.section")}
               color="white"
@@ -79,7 +80,7 @@ export default function About() {
               className="text-center lg:hidden mb-[-4rem]"
             />
             <JobTimeline />
-          </section>
+          </section> */}
 
           {/* Skills */}
 
@@ -116,6 +117,97 @@ export default function About() {
               <BsArrowDownLeft className="bg-clip-text bg-gradient-to-b from-neutral-800 to-neutral-950 dark:bg-gradient-to-b dark:from-neutral-100 dark:to-neutral-200 text-4xl lg:text-6xl" />
             </div>
             <Skills />
+          </section>
+
+          <section
+            className={`container mx-auto px-4 flex flex-col items-center text-center`}
+          >
+            <DrawCircleText />
+          </section>
+
+          <div className="container mx-auto px-4 flex items-center justify-center">
+            <Image
+              src="/images/suits.png"
+              width={600}
+              height={600}
+              alt=""
+              className="text-[100vw] lg:text-[50vw] -scale-x-100 pointer-events-none"
+            />
+          </div>
+
+          <div className="container mx-auto px-4 flex items-center justify-center">
+            <Image
+              src="/images/adidas-about.png"
+              width={800}
+              height={800}
+              alt="Adidas"
+              className="object-contain -scale-x-100 pointer-events-none"
+              style={{
+                WebkitMaskImage: `
+                  linear-gradient(to left, rgba(0,0,0,1) 10%, rgba(0,0,0,0) 100%),
+                  linear-gradient(to bottom, rgba(0,0,0,1) 50%, rgba(0,0,0,0) 100%)
+      `,
+                maskImage: `
+                  linear-gradient(to left, rgba(0,0,0,1) 10%, rgba(0,0,0,0) 100%),
+                  linear-gradient(to bottom, rgba(0,0,0,1) 50%, rgba(0,0,0,0) 100%)
+  
+      `,
+                WebkitMaskComposite: "multiply",
+                maskComposite: "intersect",
+              }}
+            />
+            <Image
+              src="/images/lamborghini-about.png"
+              width={800}
+              height={800}
+              alt="Adidas"
+              className="object-contain -scale-x-100 pointer-events-none"
+              style={{
+                WebkitMaskImage: `
+                  linear-gradient(to right, rgba(0,0,0,1) 10%, rgba(0,0,0,0) 100%),
+                  linear-gradient(to bottom, rgba(0,0,0,1) 50%, rgba(0,0,0,0) 100%)
+      `,
+                maskImage: `
+                  linear-gradient(to right, rgba(0,0,0,1) 10%, rgba(0,0,0,0) 100%),
+                  linear-gradient(to bottom, rgba(0,0,0,1) 50%, rgba(0,0,0,0) 100%)
+  
+      `,
+                WebkitMaskComposite: "multiply",
+                maskComposite: "intersect",
+              }}
+            />
+          </div>
+
+          <section
+            className={`container mx-auto px-4 flex flex-col items-center text-center`}
+          >
+            <RiPokerSpadesFill
+              aria-label="Icone Poker"
+              className="text-lg md:text-xl lg:text-[12rem] text-red-600"
+            />
+
+            <Typography
+              // text={t("Skills.title")}
+              text="Serviços"
+              color="white"
+              size="xl5"
+              className="mx-auto mt-4 mb-[5rem]"
+              style={{ lineHeight: "1.5" }}
+            />
+          </section>
+
+          <section
+            className={`${containerStyles} flex flex-col items-center text-center`}
+          >
+            <div className="flex flex-col lg:flex-row justify-center lg:justify-between items-center w-full min-h-[10rem] mb-[10rem]">
+              <Typography
+                text={"Eu poderia te ajudar com..."}
+                color="white"
+                size="xl2"
+                className="w-full lg:w-[60%] text-center lg:text-start"
+              />
+              <BsArrowDownLeft className="bg-clip-text bg-gradient-to-b from-neutral-800 to-neutral-950 dark:bg-gradient-to-b dark:from-neutral-100 dark:to-neutral-200 text-4xl lg:text-6xl" />
+            </div>
           </section>
 
           {/* Services */}
